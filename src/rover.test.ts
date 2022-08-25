d3a45d5175cdd8f44632e13baca6ff2e29dc550a
Jon Leach <jon.leach@land.tech>
2022-08-25 17:26:41 +0100

    first practice

--- src/rover.test.ts
+++ src/rover.test.ts
@@ -1,7 +1,86 @@
+import { Orientations, Rover } from "./rover";
 
 describe('Rover', () => {
 
-  test('placeholder test', () => {
-    expect(true).toEqual(true)
+  test("Rover know's it's position", () => {
+
+    const rover = new Rover(1,1)
+
+    expect(rover.x).toEqual(1)
+    expect(rover.y).toEqual(1)
   });
+
+  test("Rover know's it's orientation", () => {
+
+    const rover = new Rover(1,1, Orientations.South)
+
+    expect(rover.orientation).toEqual(Orientations.South)
+  });
+
+  test.each([
+    [Orientations.North, 1, 2],
+    [Orientations.East, 2, 1],
+    [Orientations.South, 1, 0],
+    [Orientations.West, 0, 1],
+  ])("Rover can move forwards when facing %s", (orientation, expectedX, expectedY) => {
+
+    const rover = new Rover(1,1, orientation)
+
+    rover.move("f");
+
+    expect(rover.x).toEqual(expectedX)
+    expect(rover.y).toEqual(expectedY)
+  }); 
+
+  test.each([
+    [Orientations.North, 1, 0],
+    [Orientations.East, 0, 1],
+    [Orientations.South, 1, 2],
+    [Orientations.West, 2, 1],
+  ])("Rover can move backwards when facing %s", (orientation, expectedX, expectedY) => {
+
+    const rover = new Rover(1,1, orientation)
+
+    rover.move("b");
+
+    expect(rover.x).toEqual(expectedX)
+    expect(rover.y).toEqual(expectedY)
+  }); 
+
+  test.each([
+    [Orientations.North, Orientations.West],
+    [Orientations.East, Orientations.North],
+    [Orientations.South, Orientations.East],
+    [Orientations.West, Orientations.South],
+  ])("Rover can turn left when facing %s to face %s", (startingOrientations, expectedOrientations) => {
+
+    const rover = new Rover(1,1, startingOrientations)
+
+    rover.move("l");
+
+    expect(rover.orientation.toString()).toEqual(expectedOrientations.toString())
+    expect(rover.x).toEqual(1)
+    expect(rover.y).toEqual(1)
+  
+  }); 
+
+  test.each([
+    [Orientations.North, Orientations.East],
+    [Orientations.East, Orientations.South],
+    [Orientations.South, Orientations.West],
+    [Orientations.West, Orientations.North],
+  ])("Rover can turn right when facing %s to face %s", (startingOrientations, expectedOrientations) => {
+
+    const rover = new Rover(1,1, startingOrientations)
+
+    rover.move("r");
+
+    expect(rover.orientation.toString()).toEqual(expectedOrientations.toString())
+    expect(rover.x).toEqual(1)
+    expect(rover.y).toEqual(1)
+  
+  }); 
+
+  
+
 });
--- src/rover.ts
+++ src/rover.ts
@@ -1,13 +1,71 @@
-enum Orientation {
-  North,
-  East,
-  South,
-  West,
+type Orientation = {
+  toString: () => string
+  move: (rover: Rover, amount: number) => void
+  turn: (rover: Rover, amount: number) => void
 }
-class Rover {
 
-  constructor(x: number, y: number, orientation: Orientation) {
- 
+function mod(n: number, m: number) {
+  return ((n % m) + m) % m;
+}
+
+export const Orientations: Record<string, Orientation> = {
+  North: {
+    toString: () => "North",
+    move: (rover, amount) => rover.y = rover.y + amount,
+    turn: (rover, amount) => rover.orientation = compass[mod((compass.indexOf(rover.orientation) + amount), 4)]
+  },
+  East: {
+    toString: () => "East",
+    move: (rover, amount) => rover.x = rover.x + amount,
+    turn: (rover, amount) => rover.orientation = compass[mod((compass.indexOf(rover.orientation) + amount), 4)]
+  },
+  South: {
+    toString: () =>  "South",
+    move: (rover, amount) => rover.y = rover.y - amount,
+    turn: (rover, amount) => rover.orientation = compass[mod((compass.indexOf(rover.orientation) + amount), 4)]
+  },
+  West: {
+    toString: () => "West",
+    move: (rover, amount) => rover.x = rover.x - amount,
+    turn: (rover, amount) => rover.orientation = compass[mod((compass.indexOf(rover.orientation) + amount), 4)]
+  },
+}
+
+const compass = [Orientations.North, Orientations.East, Orientations.South, Orientations.West]
+
+
+export class Rover {
+  public x: number;
+  public y: number;
+  public orientation: Orientation;
+
+  constructor(
+    x: number,
+    y: number,
+    orientation: Orientation = Orientations.North,
+  ) {
+    this.x = x;
+    this.y = y;
+    this.orientation = orientation;
   }
 
+  move(direction: string) {
+
+    switch (direction) {
+      case 'f':
+        this.orientation.move(this, 1);        
+        break;
+      case 'b':
+        this.orientation.move(this, -1);
+        break;
+      case 'l':
+        this.orientation.turn(this, -1);
+        break;
+      case 'r':
+        this.orientation.turn(this, 1);
+        break;
+      default:
+        break;
+    }
+  }
 }
